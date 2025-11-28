946728de8796a49412a851f3e819159840fe9d8f
allexkap <aekpstn@gmail.com>
2025-11-28 17:45:32 +0300

    add test version

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import Cell from './Cell.vue';
+
+let size = ref(40);
+let values = generateRandomArray(7, 40);
+
+function generateRandomArray(rows: number, cols: number): number[][] {
+  return Array.from({ length: rows }, () =>
+    Array.from({ length: cols }, () => Math.random())
+  )
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <input type="range" min="10" max="200" v-model="size">
+  <div class="wrapper">
+    <div class="grid">
+      <div v-for="(row, y) in values" class="row">
+        <Cell v-for="(saturation, x) in row" :saturation :size :date="`x=${x} y=${y}`"></Cell>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  overflow-x: auto;
+}
+
+.grid {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  min-width: max-content;
+}
+
+.row {
+  display: flex;
+  gap: 4px;
+}
+</style>
---
+++ src/Cell.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { date, saturation, hue = 120, size = 10, radius = 20 } = defineProps<{
+    date: string,
+    saturation: number,
+    hue?: number
+    size?: number,
+    radius?: number
+}>()
+
+function getColor() {
+    return `hsl(${hue}, ${saturation * 100}%, 50%)`;
+}
+</script>
+
+<template>
+    <div :style="{
+        width: `${size}px`,
+        height: `${size}px`,
+        backgroundColor: getColor(),
+        borderRadius: `${radius}%`,
+    }" :title=date></div>
+</template>
+
+<style scoped></style>
